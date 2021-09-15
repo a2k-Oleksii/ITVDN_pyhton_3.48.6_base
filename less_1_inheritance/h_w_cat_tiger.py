a394b78084d4bd0b872fd7a31b165869440eeb7c
a2k <a2k_oleksii@ukr.net>
2021-09-15 22:13:47 +0300

    homework: create two classes Cat and Tiger. Tiger inheritance in the Cat class and override her method set_size

---
+++ less_1_inheritance/h_w_cat_tiger.py
@@ -0,0 +1,35 @@
+class Cat:
+
+    def __init__(self, color, cat_type):
+        self.size = ''
+        self.color = color
+        self.cat_type = cat_type
+
+    def set_size(self):
+        if self.cat_type == 'indoor':
+            self.size = 'small'
+        else:
+            self.size = 'underfined'
+
+
+class Tiger(Cat):
+    def set_size(self):
+        if self.cat_type == 'wild':
+            self.size = 'big'
+        else:
+            self.size = 'underfined'
+
+
+cat = Cat(color='white', cat_type='british')
+cat2 = Cat(color='black', cat_type='indoor')
+cat.set_size()
+cat2.set_size()
+print(cat.size)
+print(cat2.size)
+
+tiger = Tiger(color='white', cat_type='wild')
+tiger2 = Tiger(color='black', cat_type='indoor')
+tiger.set_size()
+tiger2.set_size()
+print(tiger.size)
+print(tiger2.size)
