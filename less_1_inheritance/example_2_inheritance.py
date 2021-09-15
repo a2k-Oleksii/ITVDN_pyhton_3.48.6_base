b0e258c8034a7ca0f8dfee90d0a06a09c403f043
a2k <a2k_oleksii@ukr.net>
2021-09-15 20:12:51 +0300

    add classes A, B, C and study multi inheritance

--- less_1_inheritance/example_2_inheritance.py
+++ less_1_inheritance/example_2_inheritance.py
@@ -3,11 +3,13 @@ class Human:
         self.age = age
 
     def say_hello(self):
-        print("Hello, I am {}".format(self.age))
+        print("Hello, I am {}".format(
+            self.age
+        ))
 
 
-human_1 = Human(age=35)
-human_1.say_hello()
+# human_1 = Human(age=35)
+# human_1.say_hello()
 
 
 class HumanExtended(Human):
@@ -16,8 +18,31 @@ class HumanExtended(Human):
         self.name = name
 
     def say_hello(self):
-        print("Hello! I am {} and I am {}".format(self.name, self.age))
+        print("Hello! I am {} and I am {}".format(
+            self.name, self.age
+        ))
 
 
-human_2 = HumanExtended(age=22, name='Jhon')
-human_2.say_hello()
+# human_2 = HumanExtended(age=22, name='Jhon')
+# human_2.say_hello()
+
+
+class A:
+    def __init__(self):
+        self.a = 10
+
+
+class B:
+    def __init__(self):
+        self.b = 5
+
+
+class C(A, B):
+    def __init__(self):
+        A.__init__(self)
+        B.__init__(self)
+
+
+c = C()
+print(c.a)
+print(c.b)
