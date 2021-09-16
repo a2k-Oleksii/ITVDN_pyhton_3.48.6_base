8af0741ef2410cc137f54ddb088dcd5aad06aa4f
a2k <a2k_oleksii@ukr.net>
2021-09-16 20:06:09 +0300

    example use getter and setter

---
+++ less_2_incapsulation/example_1_user.py
@@ -0,0 +1,36 @@
+class User:
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+
+class Worker:
+    RIHTS = 'Equal'
+    SALARY_MAP = {
+        'A': 100,
+        'B': 200,
+        'C': 500
+    }
+
+    def __init__(self, working_class):
+        self.__salary = self.__get_salary(working_class)
+
+    def __get_salary(self, working_class):
+        return self.SALARY_MAP.get(working_class, 0)
+
+    @property
+    def salary(self):
+        return self.__salary
+
+
+w1 = Worker('A')
+print(w1.salary)
+w2 = Worker('C')
+print(w2.salary)
