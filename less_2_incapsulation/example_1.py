41ab3bc67deec290243b4a24076dda0c818d0665
a2k <a2k_oleksii@ukr.net>
2021-09-16 20:05:35 +0300

    example use private variables adm function

---
+++ less_2_incapsulation/example_1.py
@@ -0,0 +1,18 @@
+class Test:
+    def __init__(self, test_value):
+        self.__public_attr = test_value
+
+    def get_privet_attr(self):
+        return self.__public_attr
+
+    @staticmethod
+    def __private_function():
+        print('I private function')
+
+    def call_private_function(self):
+        self.__private_function()
+
+
+test = Test(test_value=10)
+print(test.get_privet_attr())
+test.call_private_function()
