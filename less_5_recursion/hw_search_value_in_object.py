c15cf61e65de5e653772372e8d419eab5b86cbd9
a2k-Oleksii <a2k-oleksii@ukr.net>
2021-10-23 18:28:35 +0300

    Create method from search variable inthe lest lists

--- less_5_recursion/hw_search_value_in_object.py
+++ less_5_recursion/hw_search_value_in_object.py
@@ -11,20 +11,8 @@ def search_varieble(object, variable):
                 search_varieble(element, variable)
             elif element == variable:
                 print("Searching element is find :)")
-                break
-
-
-def find_variable(array, search, index=0):
-    if index < len(array) and type(array[index]) is list:
-        find_variable(array[index], search, index=0)
-    elif index < len(array):
-        if array[index] == search:
-            print("Searching element is find :)")
-        index += 1
-        find_variable(array, search, index)        
+                break  
 
 
 list_value = [1, 2, [3, 4, [5, [6, []]]], 8]
 search_varieble(list_value, 8)
-    
-find_variable(list_value, 8)
